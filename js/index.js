2e29eba1e4d3178fbb18754783f99574842aeace
Aiden-Halliday <ashallid@lakeheadu.ca>
2025-03-15 23:35:41 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -8,10 +8,10 @@ function Hamburger(bunType, pattyAmount, pattyType, cheeseType, sauces, toppings
     this.sauces = sauces; //variable for sauce
     this.toppings = toppings; //variable for toppings
     this.burgerDetails = function () { //returns all the details of the burger
-        return `This hamburger has a ${this.bunType} bun, ${this.pattyAmount} ${this.pattyType} patty(s), with ${this.cheeseType} cheese, a sauce of ${this.sauces}, and a topping of ${this.toppings}.`;
+        return `This hamburger has a ${this.bunType} bun, ${this.pattyAmount} ${this.pattyType} patty(s), with ${this.cheeseType} cheese, a sauce of ${this.sauces}, and toppings of ${this.toppings.join(', ')}.`;
     }
 }
 
-let burger1 = new Hamburger("regular", "double", "beef", "swiss", "mustard", "pickles"); //creates an instance of the burger object
+let burger1 = new Hamburger("regular", "double", "beef", "swiss", "mustard", ["pickles", "onions", "lettuce"]); // creates an instance of the burger object with multiple toppings
 output.textContent = burger1.burgerDetails(); //displays the output to the p tag
 
